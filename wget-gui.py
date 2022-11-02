88e1956bb5033b5bb3f0ac311085eaa5a390b18c
Marko Aleksic <m.aleksic@outlook.com>
2022-11-02 15:36:47 +0100

    python file

---
+++ wget-gui.py
@@ -0,0 +1,15 @@
+import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
+
+
+def on_activate(app):
+
+    win = Gtk.ApplicationWindow(application=app)
+    win.set_title('Simple')
+    win.present()
+
+
+app = Gtk.Application(application_id='com.zetcode.Simple')
+app.connect('activate', on_activate)
+app.run(None)
